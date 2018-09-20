0858384f0b1dd787d4638d54d075656259e32d71
Arthur N <arthur@sysless.com>
2018-09-20 19:06:28 +0200

    Fix aws-mfa script

--- aws-mfa.sh
+++ aws-mfa.sh
@@ -2,9 +2,10 @@
 
 create_session_profile() {
     MFA_PROFILE="${1}-mfa"
-    SESSION_ACCESS_KEY=$(cat ~/.aws/cli/cache/${1}* | jq -r '.Credentials.AccessKeyId')
-    SESSION_SECRET_KEY=$(cat ~/.aws/cli/cache/${1}* | jq -r '.Credentials.SecretAccessKey')
-    SESSION_TOKEN=$(cat ~/.aws/cli/cache/${1}* | jq -r '.Credentials.SessionToken')
+    USER_ID=$(aws sts get-caller-identity | jq -r '.UserId')
+    SESSION_ACCESS_KEY=$(grep -h "${USER_ID}" ~/.aws/cli/cache/* | jq -r '.Credentials.AccessKeyId')
+    SESSION_SECRET_KEY=$(grep -h "${USER_ID}" ~/.aws/cli/cache/* | jq -r '.Credentials.SecretAccessKey')
+    SESSION_TOKEN=$(grep -h "${USER_ID}" ~/.aws/cli/cache/* | jq -r '.Credentials.SessionToken')
 
     aws configure set aws_access_key_id "$SESSION_ACCESS_KEY" --profile "$MFA_PROFILE"
     aws configure set aws_secret_access_key "$SESSION_SECRET_KEY" --profile "$MFA_PROFILE"
@@ -13,7 +14,6 @@ create_session_profile() {
 
 switch_profile() {
     echo "Switching profile from $AWS_PROFILE to $1"
-    export AWS_DEFAULT_PROFILE=$1
     export AWS_PROFILE=$1
     export RPROMPT="<aws:$AWS_PROFILE>"
 }
@@ -26,7 +26,6 @@ fi
 if [ -z "$AWS_PROFILE" ]; then
     echo 'No profile set'
 else
-    aws sts get-caller-identity # refresh token
     create_session_profile "$AWS_PROFILE" $2
     switch_profile "${AWS_PROFILE}-mfa"
 fi
