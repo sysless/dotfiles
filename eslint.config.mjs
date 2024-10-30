a4e875f1ee83e7f8d6e2eeb142ace50b137b1f6f
Arthur Lorotte de Banes <arthur@sysless.com>
2024-10-30 13:26:56 +0000

    Add eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  prettierConfig,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);
