38751959ac169fad33d548cb52ee407bc77682b2
Arthur Lorotte de Banes <arthur@sysless.com>
2025-05-03 16:18:16 +0000

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,21 +9,29 @@ if [ "$USER" == "root" ]; then
 fi
 
 if [ -d ~/.dotfiles ]; then
-  export DOTFILES="~/.dotfiles"
+  export DOTFILES="$HOME/.dotfiles"
 elif [ -d /workspaces/.codespaces/.persistedshare/dotfiles ]; then
   export DOTFILES="/workspaces/.codespaces/.persistedshare/dotfiles"
-  ln -sf $DOTFILES ~/.dotfiles
+  ln -sf "$DOTFILES" "$HOME/.dotfiles"
 fi
+echo "Using dotfiles at $DOTFILES"
 
 [ -f "/usr/bin/zsh" ] && sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
 
 # symlink common files
-[ ! -L ~/.zshrc ] && ln -sf $DOTFILES/.zshrc ~/.zshrc
-[ ! -L ~/.gitconfig ] && ln -sf $DOTFILES/.gitconfig ~/
-[ ! -L ~/.eslintrc.js ] && ln -sf $DOTFILES/.eslintrc.js ~/
+ln -sf "$DOTFILES/.zshrc" "$HOME/"
+ln -sf "$DOTFILES/.gitconfig" "$HOME/"
+ln -sf "$DOTFILES/.eslintrc.js" "$HOME/"
 
 # AWS config
-[ ! -d ~/.aws ] && mkdir ~/.aws
-[ ! -d ~/.awsvault/keys ] && mkdir -p ~/.awsvault/keys
+[ ! -d "$HOME/.aws" ] && mkdir "$HOME/.aws"
+[ ! -d "$HOME/.awsvault/keys" ] && mkdir -p "$HOME/.awsvault/keys"
+
+# code cli
+if [ -d "/home/$USER/.vscode-server/bin" ]; then
+  export PATH="/home/$USER/.vscode-server/bin/*/bin:$PATH"
+elif [ -d "/home/$USER/.vscode-server-insiders/bin" ]; then
+  export PATH="/home/$USER/.vscode-server-insiders/bin/*/bin:$PATH"
+fi
 
 exit 0
