c45a637c6015903236abed5177e3ee0a59cc4d3b
Arthur Lorotte de Banes <arthur@sysless.com>
2025-05-16 20:12:21 +0200

    Remove devtools from macos setup

--- setup.sh
+++ setup.sh
@@ -17,47 +17,18 @@ if [ `uname` == "Darwin" ]; then
   # Brew packages
   [ ! -f /usr/local/bin/brew ] && [ ! -f /opt/homebrew/bin/brew ] && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   export PATH=$PATH:/opt/homebrew/bin
-  brew install jq nvm yarn
-
-  # Node
-  source $(brew --prefix nvm)/nvm.sh
-  nvm install --lts # Install the latest LTS version
-
-  # Sharp (https://github.com/lovell/sharp/issues/2460#issuecomment-740467735)
-  # brew install pkg-config glib zlib vips libjpeg-turbo libpng webp mas rbenv ruby-build pyenv
-
-  # Ruby global
-  brew install rbenv ruby-build
-
-  # Python global
-  brew install python pyenv
-  #pyenv install 3.10.8
-  #pyenv global 3.10.8
 
   # Cask
   brew install --cask raycast google-chrome vlc spotify android-file-transfer discord whatsapp firefox superhuman elgato-control-center grammarly-desktop notion-calendar
   # Cask Dev
-  brew install --cask visual-studio-code aws-vault figma google-cloud-sdk docker
-  # Cask 3D printing
-  softwareupdate --install-rosetta --agree-to-license # required for autodesk-fusion
-  brew install --cask bambu-studio # autodesk-fusion
+  brew install --cask visual-studio-code figma docker
   # Mac App Store
   brew install mas
   mas install 803453959 # Slack
   mas install 497799835 # Xcode
   mas install 425424353 # The Unarchiver
   mas install 1193539993 # Brother iPrint&Scan
-  mas install 1475387142 # Tailscale
   mas upgrade
-
-  # React Native
-  # brew install android-studio android-platform-tools watchman fastlane cocoapods
-  # brew tap homebrew/cask-versions
-  # brew install zulu8
-
-  # DBT
-  # brew tap dbt-labs/dbt
-  # brew install dbt-bigquery
 
   [ ! -L ~/.myzshrc ] && ln -sf $DOTFILES/.myzshrc_mac ~/.myzshrc
   [ "$SHELL" != "/bin/zsh" ] && chpass -s /bin/zsh
